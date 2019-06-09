d910b1c30274f38fa4f2e964b1ba7b98e2d64b22
Chris Walker <tofer.walker@gmail.com>
2019-06-08 19:07:18 -0500

    Fixed backingup files within subdirectories and pulling down submodules

--- homeRepot.sh
+++ homeRepot.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
-git clone --bare $2 $HOME/.homeRepot
+git clone --bare $1 $HOME/.homeRepot
 function homeRepot {
-   /usr/bin/git --git-dir=$HOME/.homeRepot/ --work-tree=$HOME $@
+	/usr/bin/git --git-dir=$HOME/.homeRepot/ --work-tree=$HOME $@
 }
-mkdir -p .homeRepotInstall-backup
-homeRepot checkout
+homeRepot checkout 
 if [ $? = 0 ]; then
-  echo "Checked out homeRepot.";
-  else
-    echo "Backing up pre-existing dot files.";
-    homeRepot checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .homeRepotInstall-backup/{}
+	echo "Checked out homeRepot.";
+else
+	echo "Backing up pre-existing dot files.";
+	FILES=`homeRepot checkout 2>&1 | egrep "\s+\." | awk {'print $1'}`
+	for FILE in $FILES
+	do
+		#echo "File: $FILE"
+		DIRNAME=`dirname $FILE`
+		#echo "Dirname: $DIRNAME"
+		BACKUPDIR=.homeRepotInstallBackup/$DIRNAME
+		#echo "Backupdir: $BACKUPDIR"
+		mkdir -p $BACKUPDIR
+		mv $FILE $BACKUPDIR
+	done
 fi;
 homeRepot checkout
+homeRepot submodule init
+homeRepot submodule update --recursive
 homeRepot config --local status.showUntrackedFiles no
