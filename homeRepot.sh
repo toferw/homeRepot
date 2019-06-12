c53ebf6724d3c3aa763105350ef8e0662621c541
Chris Walker <tofer.walker@gmail.com>
2019-06-11 19:53:19 -0500

    Fixed commands to work with git submodules

--- homeRepot.sh
+++ homeRepot.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
 
-git clone --bare $1 $HOME/.homeRepot
+git clone --bare --recurse-submodules $1 $HOME/.homeRepot
 function homeRepot {
-	/usr/bin/git --git-dir=$HOME/.homeRepot/ --work-tree=$HOME $@
+	/usr/bin/git --git-dir=$HOME/.homeRepot --work-tree=$HOME $@
 }
-homeRepot checkout 
+homeRepot checkout
 if [ $? = 0 ]; then
 	echo "Checked out homeRepot.";
 else
 	echo "Backing up pre-existing dot files.";
-	FILES=`homeRepot checkout 2>&1 | egrep "\s+\." | awk {'print $1'}`
+	FILES=`homeRepot checkout 2>&1 | egrep "^\s+.+" | awk {'print $1'}`
 	for FILE in $FILES
 	do
 		#echo "File: $FILE"
 		DIRNAME=`dirname $FILE`
 		#echo "Dirname: $DIRNAME"
-		BACKUPDIR=.homeRepotInstallBackup/$DIRNAME
+		BACKUPDIR=.backupHomeRepotInstall/$DIRNAME
 		#echo "Backupdir: $BACKUPDIR"
 		mkdir -pv $BACKUPDIR
 		mv -v $FILE $BACKUPDIR
 	done
-fi;
-homeRepot checkout
+	echo "Trying again...";
+	homeRepot checkout
+	if [ $? = 0 ]; then
+		echo "Checked out homeRepot.";
+	else
+		echo "Things went horribly, horribly wrong...";
+	fi;
 homeRepot submodule init
-homeRepot submodule update --recursive
+homeRepot submodule update
 homeRepot config --local status.showUntrackedFiles no
+fi;
